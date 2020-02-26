1c87d7ff88c009a6badc73acc59fbdc3f6d31c86
mulonemartin <mulone.martin@gmail.com>
2020-02-26 20:41:43 -0300

    scripts

--- node_manager/tests/reserve_balance_of.py
+++ node_manager/tests/reserve_balance_of.py
@@ -3,7 +3,7 @@ from web3 import Web3
 from node_manager.utils import NodeManager
 
 
-network = 'mocMainnet'
+network = 'development'
 config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'config.json')
 node_manager = NodeManager(path_to_config=config_path, network=network)
 node_manager.connect_node()
@@ -30,7 +30,7 @@ moc_state = node_manager.load_json_contract(os.path.join(path_build, "MoCState.j
                                             deploy_address=moc_state_address)
 
 print("Reserve token balance:")
-from_address = Web3.toChecksumAddress("0xea14C08764c9E5F212C916E11a5C47eaF92571E4")
+from_address = Web3.toChecksumAddress("0xCD8A1c9aCc980ae031456573e34dC05cD7daE6e3")
 
 balance = reserve_token.functions.balanceOf(from_address).call()
 print("Account: {0} Reserve Balance: {1}".format(from_address, Web3.fromWei(balance, 'ether')))
@@ -65,12 +65,28 @@ print("getStablePower: {0} inrateTPl: (BtcxTmax, BtcxPower, BtcxTmin )".format(r
 result = moc_inrate.functions.getStableTmin().call()
 print("getStableTmin: {0} inrateTPl: (BtcxTmax, BtcxPower, BtcxTmin )".format(result))
 
-interest_v = moc_inrate.functions.calcMintInterestValues(str.encode('X2'), 10000000000000000).call()
-print("Interest: {0}".format(interest_v))
 
-interest_v = moc_inrate.functions.riskProxInrateAvg(str.encode('X2'), 1 * 10 ** 18, False).call()
-print("Interest riskproxinrateavg: {0}".format(Web3.fromWei(interest_v, 'ether')))
+#result = moc_state.functions.abundanceRatio(1000000000000000000).call()
+#print("Abundance 1: {0}".format(result))
+
+# interest_v = moc_inrate.functions.riskProxInrateAvg(str.encode('X2'), 1000000000000000000, False).call()
+# print("Interest AVG: {0}".format(interest_v))
+#
+interest_v = moc_inrate.functions.calcMintInterestValues(str.encode('X2'), 1000000000000000000).call()
+print("Interest: {0}".format(interest_v))
 
 result = moc_inrate.functions.stableTokenInrateAvg(0).call()
 print("stableTokenInrateAvg: {0}".format(result))
 
+#mocPrecision = moc_moc.functions.getMocPrecision().call()
+#claim_value = 1000
+
+# tx_hash = node_manager.fnx_transaction(reserve_token, 'claim', int(claim_value * mocPrecision))
+# tx_receipt = node_manager.wait_transaction_receipt(tx_hash)
+# print(tx_receipt)
+#
+#
+# print("Reserve token balance:")
+# from_address = Web3.toChecksumAddress(node_manager.options['networks'][network]['accounts'][0]['address'])
+# balance = reserve_token.functions.balanceOf(from_address).call()
+# print("Account: {0} Balance: {1}".format(from_address, Web3.fromWei(balance, 'ether')))
