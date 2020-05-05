a1895e8cf278f15c510a263069a45aa09bf6b867
mulonemartin <mulone.martin@gmail.com>
2020-05-05 08:58:40 -0300

    Contract changers added

---
+++ contracts/changers/MoCInrateRiskproxChanger.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.5.8;
+import "../MoCInrate.sol";
+import "moc-governance/contracts/Governance/ChangeContract.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+/**
+ * @dev This contract is used to update the configuration of MocInrate
+ * with MoC --- governance.
+ */
+
+contract MoCInrateRiskproxChanger is ChangeContract, Ownable, MoCInrateStructs{
+  MoCInrate private mocInrate;
+
+  constructor(
+    MoCInrate _mocInrate,
+    uint256 riskProxMin,
+    uint256 riskProxMax,
+    uint256 riskProxPower
+  ) public {
+    mocInrate = _mocInrate;
+    riskProxParams.tMin = riskProxMin;
+    riskProxParams.power = riskProxPower;
+    riskProxParams.tMax = riskProxMax;
+  }
+
+  function execute() external {
+    mocInrate.setRiskProxTmin(riskProxParams.tMin);
+    mocInrate.setRiskProxTmax(riskProxParams.tMax);
+    mocInrate.setRiskProxPower(riskProxParams.power);
+  }
+
+  function setRiskProxTmin(uint256 _btxcTmin) public onlyOwner() {
+    riskProxParams.tMin = _btxcTmin;
+  }
+
+  function setRiskProxTmax(uint256 _btxcTax) public onlyOwner() {
+    riskProxParams.tMax = _btxcTax;
+  }
+
+  function setRiskProxPower(uint256 _btxcPower) public onlyOwner() {
+    riskProxParams.power = _btxcPower;
+  }
+
+
+}
---
+++ contracts/changers/MoCInrateStableChanger.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.5.8;
+import "../MoCState.sol";
+import "moc-governance/contracts/Governance/ChangeContract.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+/**
+ * @dev This contract is used to update the configuration of MocInrate
+ * with MoC --- governance.
+ */
+contract MocInrateStableChanger is ChangeContract, Ownable{
+  MoCInrate private mocInrate;
+  uint256 public stableTmin;
+  uint256 public stableTmax;
+  uint256 public stablePower;
+
+  constructor(
+    MoCInrate _mocInrate,
+    uint256 _stableTmin,
+    uint256 _stableTmax,
+    uint256 _stablePower
+  ) public {
+    mocInrate = _mocInrate;
+    stableTmin = _stableTmin;
+    stableTmax = _stableTmax;
+    stablePower = _stablePower;
+  }
+
+  function execute() external {
+    mocInrate.setStableTmin(stableTmin);
+    mocInrate.setStableTmax(stableTmax);
+    mocInrate.setStablePower(stablePower);
+  }
+
+  function setStableTmin(uint256 _stableTmin) public onlyOwner(){
+    stableTmin = _stableTmin;
+  }
+
+  function setStableTmax(uint256 _stableTmax) public onlyOwner(){
+    stableTmax = _stableTmax;
+  }
+
+  function setStablePower(uint256 _stablePower) public onlyOwner(){
+    stablePower = _stablePower;
+  }
+
+}
