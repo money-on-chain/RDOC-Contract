fa60c2f7279261d455c3fb277df3ce25ca371b24
pedro <pedro@thinkanddev.com>
2021-08-07 23:22:13 -0300

    Batch Changer Use the BatchChanger contract to perform all the updates and contract calls in one transaction Remove contracts/changers/deploy as we don't need other changers any more Updated deploy scripts Test of BatchChanger are at money-on-chain/Areopagus-Governance#22 Script 10_batch_changer.js could use a refactor, and separe it in methods to avoid a the current wall of text Currently BatchChanger.sol allows to add more transactions after the deploy. Maybe we want to change to use only the constructor as the BulkUpgrade

--- scripts/deploy/20210308/7_deploy_MoCInrate.js
+++ scripts/deploy/20210308/7_deploy_MoCInrate.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-console */
-const UpgraderChanger = artifacts.require('./changers/UpgraderChanger.sol');
-const Governor = artifacts.require('moc-governance/contracts/Governance/Governor.sol');
-
 const MoCInrate = artifacts.require('./MoCInrate.sol');
-const MoCInrateChangerDeploy = artifacts.require('./MocInrateChangerDeploy.sol');
 
 const BigNumber = require('bignumber.js');
 const { getConfig, getNetwork, saveConfig, shouldExecuteChanges } = require('../helper');
@@ -102,45 +98,9 @@ module.exports = async callback => {
     console.log('Deploy MoCInrate');
     const mocInrate = await MoCInrate.new();
 
-    // Upgrade contracts with proxy (using the contract address of contract just deployed)
-    console.log('Upgrade MoCInrate');
-    const upgradeMocInrate = await UpgraderChanger.new(
-      config.proxyAddresses.MoCInrate,
-      config.implementationAddresses.UpgradeDelegator,
-      mocInrate.address
-    );
-
     // Save implementation address and changer address to config file
     config.implementationAddresses.MoCInrate = mocInrate.address;
-    config.changerAddresses['7_MoCInrate'] = upgradeMocInrate.address;
     saveConfig(config, configPath);
-
-    let governor;
-    if (shouldExecuteChanges(network)) {
-      // Execute changes in contracts
-      console.log('Execute change - MoCInrate');
-      governor = await Governor.at(config.implementationAddresses.Governor);
-      await governor.executeChange(upgradeMocInrate.address);
-    }
-
-    // Setting commissions
-    const commissions = await getCommissionsArray(mocInrate)(config);
-
-    // Use changer contract
-    const mocInrateChangerDeploy = await MoCInrateChangerDeploy.new(
-      config.proxyAddresses.MoCInrate,
-      commissions
-    );
-
-    // Save changer address to config file
-    config.changerAddresses['7_MoCInrateChangerDeploy'] = mocInrateChangerDeploy.address;
-    saveConfig(config, configPath);
-
-    if (shouldExecuteChanges(network)) {
-      // Execute changes in MoCInrate
-      console.log('Execute change - MoCInrateChangerDeploy');
-      await governor.executeChange(mocInrateChangerDeploy.address);
-    }
 
     console.log('MoCInrate implementation address: ', mocInrate.address);
   } catch (error) {
